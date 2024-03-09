078d5f1dbfc2c0a2cb466666349dbf1bd64a61f3
nikhil03a <anikhil03prasanna@gmail.com>
2024-03-09 14:32:54 +0530

    change

--- src/config/Ip.js
+++ src/config/Ip.js
@@ -1,4 +1,4 @@
-let ip = "https://fantasy-league-backend-theta.vercel.app/";
+let ip = "https://fantasy-league-backend-theta.vercel.app";
 // let ip = "http://10.10.113.28:5000"
 
 export default ip;
