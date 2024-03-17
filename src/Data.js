86ca4f91bd190190b359ed38d99724b0ca1b05af
nikhil03a <anikhil03prasanna@gmail.com>
2024-03-17 20:53:33 +0530

    change

--- src/Data.js
+++ src/Data.js
@@ -1,262 +1,263 @@
 const data = [{
-   "_id": {
-     "$oid": "65f6e265f2c1a29d9340537e"
-   },
-   "id": "1",
-   "team1": "Chennai Super Kings",
-   "team2": "Royal Challengers Bangalore",
-   "toss": " CSK won the toss and elected to bat first",
-   "stadium": "MA Chidambaram Stadium, Chennai",
-   "totalRunsTeam1": "209/4",
-   "totalRunsTeam2": "164/6",
-   "result": "CSK won by 45 runs",
-   "battingTeam1": [
-     {
-       "id": "1",
-       "player": "Rachin Ravindra",
-       "runs": 27,
-       "balls": 26,
-       "fours": 4,
-       "sixes": 1,
-       "strikeRate": 103.85
-     },
-     {
-       "id": "2",
-       "player": "Ruturaj Gaikwad",
-       "runs": 80,
-       "balls": 56,
-       "fours": 7,
-       "sixes": 5,
-       "strikeRate": 142.85
-     },
-     {
-       "id": "10",
-       "player": "Ajinkya Rahane",
-       "runs": 14,
-       "balls": 6,
-       "fours": 3,
-       "sixes": 0,
-       "strikeRate": 233.33
-     },
-     {
-       "id": "9",
-       "player": "Ajinkya Rahane",
-       "runs": 53,
-       "balls": 29,
-       "fours": 4,
-       "sixes": 4,
-       "strikeRate": 182.76
-     },
-     {
-       "id": "0",
-       "player": "Mahendra Singh Dhoni",
-       "runs": 20,
-       "balls": 7,
-       "fours": 0,
-       "sixes": 0,
-       "strikeRate": 285.71
-     },
-     {
-       "id": "3",
-       "player": "Ravindra Jadeja",
-       "runs": 0,
-       "balls": 0,
-       "fours": 0,
-       "sixes": 0,
-       "strikeRate": 0
-     }
-   ],
-   "bowlingTeam1": [
-     {
-       "id": "30",
-       "player": "Akash Deep",
-       "overs": 4,
-       "maidens": 0,
-       "runs_": 31,
-       "wickets": 0,
-       "economy": 7.75
-     },
-     {
-       "id": "28",
-       "player": "Alzari Joseph",
-       "overs": 4,
-       "maidens": 0,
-       "runs_": 34,
-       "wickets": 1,
-       "economy": 8.5
-     },
-     {
-       "id": "26",
-       "player": "Glenn Maxwell",
-       "overs": 3,
-       "maidens": 0,
-       "runs_": 46,
-       "wickets": 0,
-       "economy": 15.33
-     },
-     {
-       "id": "27",
-       "player": "Vijaykumar Vyshak",
-       "overs": 4,
-       "maidens": 0,
-       "runs_": 51,
-       "wickets": 0,
-       "economy": 12.75
-     },
-     {
-       "id": "24",
-       "player": "Mohammed Siraj",
-       "overs": 4,
-       "maidens": 0,
-       "runs_": 29,
-       "wickets": 0,
-       "economy": 7.25
-     },
-     {
-       "id": "32",
-       "player": "Cameron Green",
-       "overs": 1,
-       "maidens": 0,
-       "runs_": 11,
-       "wickets": 0,
-       "economy": 11
-     }
-   ],
-   "extrasTeam1": 15,
-   "battingTeam2": [
-     {
-       "id": "23",
-       "player": "Virat Kohli",
-       "runs": 53,
-       "balls": 42,
-       "fours": 1,
-       "sixes": 3,
-       "strikeRate": 126.19
-     },
-     {
-       "id": "22",
-       "player": "Faf du Plessis",
-       "runs": 26,
-       "balls": 18,
-       "fours": 5,
-       "sixes": 0,
-       "strikeRate": 144.44
-     },
-     {
-       "id": "32",
-       "player": "Cameron Green",
-       "runs": 5,
-       "balls": 8,
-       "fours": 1,
-       "sixes": 0,
-       "strikeRate": 62.5
-     },
-     {
-       "id": "29",
-       "player": "Rajat Patidar",
-       "runs": 15,
-       "balls": 11,
-       "fours": 0,
-       "sixes": 1,
-       "strikeRate": 136.36
-     },
-     {
-       "id": "26",
-       "player": "Glenn Maxwell",
-       "runs": 22,
-       "balls": 15,
-       "fours": 0,
-       "sixes": 2,
-       "strikeRate": 146.67
-     },
-     {
-       "id": "25",
-       "player": "Mahipal Lomror",
-       "runs": 4,
-       "balls": 6,
-       "fours": 0,
-       "sixes": 0,
-       "strikeRate": 66.67
-     },
-     {
-       "id": "31",
-       "player": "Dinesh Karthik",
-       "runs": 26,
-       "balls": 18,
-       "fours": 1,
-       "sixes": 1,
-       "strikeRate": 144.44
-     },
-     {
-       "id": "27",
-       "player": "Vijaykumar Vyshak",
-       "runs": 2,
-       "balls": 2,
-       "fours": 0,
-       "sixes": 0,
-       "strikeRate": 100
-     }
-   ],
-   "bowlingTeam2": [
-     {
-       "id": "4",
-       "player": "Matheesha Pathirana",
-       "overs": 4,
-       "maidens": 0,
-       "runs_": 31,
-       "wickets": 0,
-       "economy": 7.75
-     },
-     {
-       "id": "7",
-       "player": "Deepak Chahar",
-       "overs": 4,
-       "maidens": 0,
-       "runs_": 36,
-       "wickets": 2,
-       "economy": 9
-     },
-     {
-       "id": "8",
-       "player": "Tushar Deshpande",
-       "overs": 3,
-       "maidens": 0,
-       "runs_": 26,
-       "wickets": 1,
-       "economy": 8.67
-     },
-     {
-       "id": "5",
-       "player": "Moeen Ali",
-       "overs": 4,
-       "maidens": 0,
-       "runs_": 22,
-       "wickets": 1,
-       "economy": 5.5
-     },
-     {
-       "id": "3",
-       "player": "Ravindra Jadeja",
-       "overs": 4,
-       "maidens": 0,
-       "runs_": 25,
-       "wickets": 2,
-       "economy": 6.25
-     },
-     {
-       "id": "6",
-       "player": "Shardul Thakur",
-       "overs": 1,
-       "maidens": 0,
-       "runs_": 17,
-       "wickets": 0,
-       "economy": 17
-     }
-   ],
-   "extrasTeam2": 11,
-   "fallOfWicketsTeam1": [],
-   "fallOfWicketsTeam2": [],
-   "__v": 0,
-   "team1flag": "https://i.pinimg.com/564x/ea/40/fd/ea40fd13fa7d46618c5567146f07ba41.jpg",
-   "team2flag": "https://logotyp.us/file/royal-challengers.svg"
- }]
+  "_id": {
+    "$oid": "65f6e265f2c1a29d9340537e"
+  },
+  "id": "1",
+  "team1": "Chennai Super Kings",
+  "team2": "Royal Challengers Bangalore",
+  "toss": " CSK won the toss and elected to bat first",
+  "stadium": "MA Chidambaram Stadium, Chennai",
+  "totalRunsTeam1": "209/4",
+  "totalRunsTeam2": "164/6",
+  "result": "CSK won by 45 runs",
+  "battingTeam1": [
+    {
+      "id": "1",
+      "player": "Rachin Ravindra",
+      "runs": 27,
+      "balls": 26,
+      "fours": 4,
+      "sixes": 1,
+      "strikeRate": 103.85
+    },
+    {
+      "id": "2",
+      "player": "Ruturaj Gaikwad",
+      "runs": 80,
+      "balls": 56,
+      "fours": 7,
+      "sixes": 5,
+      "strikeRate": 142.85
+    },
+    {
+      "id": "10",
+      "player": "Ajinkya Rahane",
+      "runs": 14,
+      "balls": 6,
+      "fours": 3,
+      "sixes": 0,
+      "strikeRate": 233.33
+    },
+    {
+      "id": "9",
+      "player": "Ajinkya Rahane",
+      "runs": 53,
+      "balls": 29,
+      "fours": 4,
+      "sixes": 4,
+      "strikeRate": 182.76
+    },
+    {
+      "id": "0",
+      "player": "Mahendra Singh Dhoni",
+      "runs": 20,
+      "balls": 7,
+      "fours": 0,
+      "sixes": 0,
+      "strikeRate": 285.71
+    },
+    {
+      "id": "3",
+      "player": "Ravindra Jadeja",
+      "runs": 0,
+      "balls": 0,
+      "fours": 0,
+      "sixes": 0,
+      "strikeRate": 0
+    }
+  ],
+  "bowlingTeam1": [
+    {
+      "id": "30",
+      "player": "Akash Deep",
+      "overs": 4,
+      "maidens": 0,
+      "runs_": 31,
+      "wickets": 0,
+      "economy": 7.75
+    },
+    {
+      "id": "28",
+      "player": "Alzari Joseph",
+      "overs": 4,
+      "maidens": 0,
+      "runs_": 34,
+      "wickets": 1,
+      "economy": 8.5
+    },
+    {
+      "id": "26",
+      "player": "Glenn Maxwell",
+      "overs": 3,
+      "maidens": 0,
+      "runs_": 46,
+      "wickets": 0,
+      "economy": 15.33
+    },
+    {
+      "id": "27",
+      "player": "Vijaykumar Vyshak",
+      "overs": 4,
+      "maidens": 0,
+      "runs_": 51,
+      "wickets": 0,
+      "economy": 12.75
+    },
+    {
+      "id": "24",
+      "player": "Mohammed Siraj",
+      "overs": 4,
+      "maidens": 0,
+      "runs_": 29,
+      "wickets": 0,
+      "economy": 7.25
+    },
+    {
+      "id": "32",
+      "player": "Cameron Green",
+      "overs": 1,
+      "maidens": 0,
+      "runs_": 11,
+      "wickets": 0,
+      "economy": 11
+    }
+  ],
+  "extrasTeam1": 15,
+  "battingTeam2": [
+    {
+      "id": "23",
+      "player": "Virat Kohli",
+      "runs": 53,
+      "balls": 42,
+      "fours": 1,
+      "sixes": 3,
+      "strikeRate": 126.19
+    },
+    {
+      "id": "22",
+      "player": "Faf du Plessis",
+      "runs": 26,
+      "balls": 18,
+      "fours": 5,
+      "sixes": 0,
+      "strikeRate": 144.44
+    },
+    {
+      "id": "32",
+      "player": "Cameron Green",
+      "runs": 5,
+      "balls": 8,
+      "fours": 1,
+      "sixes": 0,
+      "strikeRate": 62.5
+    },
+    {
+      "id": "29",
+      "player": "Rajat Patidar",
+      "runs": 15,
+      "balls": 11,
+      "fours": 0,
+      "sixes": 1,
+      "strikeRate": 136.36
+    },
+    {
+      "id": "26",
+      "player": "Glenn Maxwell",
+      "runs": 22,
+      "balls": 15,
+      "fours": 0,
+      "sixes": 2,
+      "strikeRate": 146.67
+    },
+    {
+      "id": "25",
+      "player": "Mahipal Lomror",
+      "runs": 4,
+      "balls": 6,
+      "fours": 0,
+      "sixes": 0,
+      "strikeRate": 66.67
+    },
+    {
+      "id": "31",
+      "player": "Dinesh Karthik",
+      "runs": 26,
+      "balls": 18,
+      "fours": 1,
+      "sixes": 1,
+      "strikeRate": 144.44
+    },
+    {
+      "id": "27",
+      "player": "Vijaykumar Vyshak",
+      "runs": 2,
+      "balls": 2,
+      "fours": 0,
+      "sixes": 0,
+      "strikeRate": 100
+    }
+  ],
+  "bowlingTeam2": [
+    {
+      "id": "4",
+      "player": "Matheesha Pathirana",
+      "overs": 4,
+      "maidens": 0,
+      "runs_": 31,
+      "wickets": 0,
+      "economy": 7.75
+    },
+    {
+      "id": "7",
+      "player": "Deepak Chahar",
+      "overs": 4,
+      "maidens": 0,
+      "runs_": 36,
+      "wickets": 2,
+      "economy": 9
+    },
+    {
+      "id": "8",
+      "player": "Tushar Deshpande",
+      "overs": 3,
+      "maidens": 0,
+      "runs_": 26,
+      "wickets": 1,
+      "economy": 8.67
+    },
+    {
+      "id": "5",
+      "player": "Moeen Ali",
+      "overs": 4,
+      "maidens": 0,
+      "runs_": 22,
+      "wickets": 1,
+      "economy": 5.5
+    },
+    {
+      "id": "3",
+      "player": "Ravindra Jadeja",
+      "overs": 4,
+      "maidens": 0,
+      "runs_": 25,
+      "wickets": 2,
+      "economy": 6.25
+    },
+    {
+      "id": "6",
+      "player": "Shardul Thakur",
+      "overs": 1,
+      "maidens": 0,
+      "runs_": 17,
+      "wickets": 0,
+      "economy": 17
+    }
+  ],
+  "extrasTeam2": 11,
+  "fallOfWicketsTeam1": [],
+  "fallOfWicketsTeam2": [],
+  "__v": 0,
+  "team1flag": "https://i.pinimg.com/564x/ea/40/fd/ea40fd13fa7d46618c5567146f07ba41.jpg",
+  "team2flag": "https://logotyp.us/file/royal-challengers.svg"
+}]
+export default data
